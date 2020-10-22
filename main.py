7ba7c8b2317ab25a3d3e8c0c5539f41b87d25ba0
sinhhn <h.sinh@bravesoft.co.jp>
2020-10-22 18:14:48 +0900

    add merge sort

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from sort import quickSort, mergeSort, printList
 
 
 def print_hi(name):
@@ -25,41 +26,17 @@ def devide(a, b):
     return a / b
 
 
-# Phương pháp cùi bắp
-def sort(arr):
-    for i in range(0, len(arr) - 1):
-        for j in range(i + 1, len(arr) - 1):
-            if arr[i] > arr[j]:
-                tmp = arr[i]
-                arr[i] = arr[j]
-                arr[j] = tmp
-
-
-def mySort(arr, low, high):
-    if len(arr) == 1:
-        return arr
-    if low < high:
-        pi = partition(arr, low, high)
-        mySort(arr, low, pi - 1)
-        mySort(arr, pi + 1, high)
-
-
-def partition(arr, low, high):
-    i = (low - 1)
-    pivot = arr[high]  # pivot
-    for j in range(low, high):
-        if arr[j] <= pivot:
-            i = i + 1
-            arr[i], arr[j] = arr[j], arr[i]
-
-    arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    return i + 1
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     array = [1, 2, 4, 6, 8, 3, 9, 4, 3, 5, 7, 4, 8, 9]
-    mySort(array, 0, len(array) - 1)
-    for element in array:
-        print(element)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("Given array is", end="\n")
+    printList(array)
+    quickSort(array, 0, len(array) - 1)
+    print("Sorted array is: ", end="\n")
+    printList(array)
+    arr = [12, 11, 13, 5, 6, 7]
+    print("Given array is", end="\n")
+    printList(arr)
+    mergeSort(arr)
+    print("Sorted array is: ", end="\n")
+    printList(arr)
