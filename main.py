a036d8321395dc11d5d64776ba7f07aed14a3851
sinhhn <h.sinh@bravesoft.co.jp>
2020-10-22 14:09:21 +0900

    add sortquick

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def multiple(a, b):
 def devide(a, b):
     return a / b
 
+
 # Phương pháp cùi bắp
 def sort(arr):
     for i in range(0, len(arr) - 1):
@@ -33,15 +34,32 @@ def sort(arr):
                 arr[i] = arr[j]
                 arr[j] = tmp
 
-def quicksort(arr):
-    
+
+def mySort(arr, low, high):
+    if len(arr) == 1:
+        return arr
+    if low < high:
+        pi = partition(arr, low, high)
+        mySort(arr, low, pi - 1)
+        mySort(arr, pi + 1, high)
+
+
+def partition(arr, low, high):
+    i = (low - 1)
+    pivot = arr[high]  # pivot
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    array = [1, 2, 4, 6, 8, 3, 9]
-    sort(array)
+    array = [1, 2, 4, 6, 8, 3, 9, 4, 3, 5, 7, 4, 8, 9]
+    mySort(array, 0, len(array) - 1)
     for element in array:
         print(element)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
