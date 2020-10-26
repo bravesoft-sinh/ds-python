03f8f04c10658deb8a163684d3f6ec86a827a63e
sinhhn <h.sinh@bravesoft.co.jp>
2020-10-26 12:57:59 +0900

    imporve with lower and upper case

--- basic.py
+++ basic.py
@@ -74,7 +74,10 @@ def translate(pharse):
     translation = ""
     for letter in pharse:
         if letter.lower() in "aeiou":
-            translation = translation + "g"
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
         else:
             translation = translation + letter
 
